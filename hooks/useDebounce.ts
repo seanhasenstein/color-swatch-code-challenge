676a5a02e46ca4648da5c8dc6c05ba48520dadaf
Sean Hasenstein <seanhasenstein@gmail.com>
2025-12-04 14:41:29 -0600

    set default value of 500 for useDebounce

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 // custom hook to debounce a value
 // delays updating the debouncedValue until after the delay to prevent excessive API calls while the user is updating the value
-export default function useDebounce<T>(value: T, delay: number): T {
+export default function useDebounce<T>(value: T, delay = 500): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
